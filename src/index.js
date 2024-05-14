035350129fab71b8d674b27f43a108d2d1f4a937
Juan Costa <juandev1998@gmail.com>
2024-05-14 16:48:01 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplicacao(2, 4));
+function saudacao(nome) {
+    return `Ola, seja-bem vindo: ${nome}`;
+}
+console.log(saudacao('juan'));
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function multiplicacao(n1: number, n2: number):number {
+    return n1 * n2
+}
+
+console.log(multiplicacao(2 , 4))
+
+
+function saudacao(nome: string):string {
+    return `Ola, seja-bem vindo: ${nome}`
+}
+
+console.log(saudacao('Juan'))
